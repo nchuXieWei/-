59c9ea18750f7c9924437c2d1ba28fa55cb93d4f
nchuXieWei <2781816683@qq.com>
2017-07-24 21:50:51 +0800

    Add files via upload

--- XWtree.h
+++ XWtree.h
@@ -22,14 +22,13 @@ typedef struct fileNode {
 }fileNode;
 
 
-
-
-
-
 //用于判断用户所选操作的枚举类型
 typedef enum Choice{
-	INSERT,PREORDER,POSTORDER,QUIT
+	INSERT,DELETE,PREORDER,POSTORDER,QUIT
 }Choice;
+
+
+
 
 //main中会使用到的函数
 bool Initialize(Tree *t); //第一次使用程序时需要的初始化函数
@@ -39,7 +38,7 @@ void PrintPreOrder(Tree t, int layer);    //先序遍历树
 void PrintPostOrder(Tree t, int layer);   //后序遍历树
 bool Load(Tree *t);    //将树从文件中加载至内存
 void Insert(Tree t);  //在树中新增结点
-void Delete(Tree t); //删除树中的某结点（若该结点为文件夹，则删除其下所有文件）
+void DeleteNode(Tree t); //删除树中的某结点（若该结点为文件夹，则删除其下所有文件）
 
 
 #endif
